5f7038c668fbd10b7ed993ce3aaf7f1589e731eb
ogmarco <ogmarco3@yahoo.com>
2025-06-25 19:51:38 -0300

    loginform

--- public/index.php
+++ public/index.php
@@ -2,3 +2,31 @@
 
 session_start();
 
+$q = $_GET['q'] ?? null;
+
+$show = true;
+
+if ($q == 'login/submit') {
+    $school_name = $_POST['school_name'];
+    $user_name = $_POST['user_name'];
+    $password  = $_POST['password'];
+    
+    if ($password == "123") {
+        echo "Login!";
+       
+        $show = false;
+
+    } else {
+    
+        echo "Access denied";
+
+       
+    }
+
+}
+
+
+if ($show == true){
+    include "../src/modules/login/view/login_form.php";
+}
+
---
+++ src/modules/login/view/login_form.php
@@ -0,0 +1,11 @@
+<form action="?q=login/submit" method="post">
+
+<label>School name</label> <br>   
+<input type="text" name="school_name"><br>
+<label>Username</label><br>
+<input type="text" name="user_name"><br>
+<label>Password</label><br>
+<input type="password" name="password"><br>
+<button type="submit">Login</button>
+
+</form>
